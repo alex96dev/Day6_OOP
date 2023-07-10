999038b4e87069172486068edec5bf6b11a0e2a3
alexanderstracke <alexander.stracke@gmx.net>
2023-07-10 10:36:36 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/Main.java
@@ -0,0 +1,16 @@
+package de.neuefische;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student(); // Student ist großgeschrieben, weil komplexer Datentyp
+
+        student.alter = 25;
+        student.studiengang = "Informatik";
+        student.semster = 35;
+
+        System.out.println(student.alter);
+        System.out.println(student.studiengang);
+        System.out.println(student.semster);
+    }
+}
---
+++ src/main/java/de/neuefische/Student.java
@@ -0,0 +1,12 @@
+package de.neuefische;
+
+public class Student {
+
+    int alter;
+
+    String studiengang;
+
+    int semster;
+
+
+}
